25b11ffe5933d3cc59a649648d56c1f08b8173a8
gilinykh <gilinykh@gramant.ru>
2018-12-06 12:49:37 +0300

    Add jdbc email provider

---
+++ src/main/java/com/gramant/notification/email/adapters/jdbc/JdbcEmailProvider.java
@@ -0,0 +1,30 @@
+package com.gramant.notification.email.adapters.jdbc;
+
+import com.gramant.notification.UserId;
+import com.gramant.notification.email.app.EmailProvider;
+import com.gramant.notification.email.domain.Email;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class JdbcEmailProvider implements EmailProvider {
+
+    // todo: make configurable
+    private static final String CONTACT_TABLE = "user_contact";
+    private static final String CONTACT_ID_COLUMN = "user_id";
+    private static final String CONTACT_EMAIL_COLUMN = "email";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Email> email(UserId userId) {
+        String query = String.format("SELECT %s FROM %s WHERE %s = ?", CONTACT_EMAIL_COLUMN, CONTACT_TABLE, CONTACT_ID_COLUMN);
+        List<String> emails = jdbcTemplate.queryForList(query, new Object[] {userId.asString()}, String.class);
+        return emails.isEmpty() ? Optional.empty() : Optional.of(Email.of(emails.get(0)));
+    }
+}
--- src/main/java/com/gramant/notification/email/domain/Email.java
+++ src/main/java/com/gramant/notification/email/domain/Email.java
@@ -1,12 +1,18 @@
 package com.gramant.notification.email.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public class Email implements Serializable {
+public final class Email implements Serializable {
 
     private final String value;
 
+    public static Email of(String value) {
+        return new Email(value);
+    }
+
     private Email(String value) {
+        Objects.requireNonNull(value);
         this.value = value;
     }
 
